e869a6c3d5569f0cb9516d5231df91c831a31483
Nicolas Fish <nicolasfish1224@gmail.com>
2020-05-07 13:10:50 -0700

    Side Scroller prototype

---
+++ Assets/SideScroller/Scripts/Player2DController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player2DController : MonoBehaviour
+{
+    public float moveRange = 8;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float newPosY = Camera.main.ScreenToViewportPoint(Input.mousePosition).y;
+        this.transform.position = new Vector3(this.transform.position.x, newPosY * moveRange, this.transform.position.z);
+    }
+}
---
+++ Assets/SideScroller/Scripts/SideScrollerSpawner.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SideScrollerSpawner : MonoBehaviour
+{
+    public Rigidbody[] ObjectsToSpawn;
+    public Vector3 spawnDirection;
+    public float force;
+
+    public float secondsBetweenSpawn = 2;
+    public float elapsedTime = 0.0f;
+
+    private List<Rigidbody> _spawedObjects;
+
+    void Start()
+    {
+        _spawedObjects = new List<Rigidbody>();
+    }
+
+    void Update()
+    {
+        elapsedTime += Time.deltaTime;
+
+        if (elapsedTime > secondsBetweenSpawn)
+        {
+            elapsedTime = 0;
+
+            Vector3 spawnPosition = this.transform.position + new Vector3(0, Random.Range(-4.0f, 4.0f), 0);
+            Rigidbody newObject;
+            newObject = Instantiate(ObjectsToSpawn[Random.Range(0, ObjectsToSpawn.Length)], spawnPosition, this.transform.rotation);
+            newObject.AddForce(spawnDirection * force);
+
+            _spawedObjects.Add(newObject);
+        }
+
+        foreach(Rigidbody spawedObject in _spawedObjects.ToArray())
+        {
+            Vector3 objectPos = spawedObject.transform.position;
+            if ((objectPos.y > 10 || objectPos.y < -10) || (objectPos.x > 15 || objectPos.y < -15))
+            {
+                _spawedObjects.Remove(spawedObject);
+                Destroy(spawedObject.gameObject);
+            }
+        }
+    }
+}
